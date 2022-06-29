2892683d6e85b70e2eb8e0e4f4941234166742f3
Crowdbotics <team@crowdbotics.com>
2022-06-29 15:02:35 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/liberty_order_35971/MainActivity.java
@@ -0,0 +1,15 @@
+package com.liberty_order_35971;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "liberty_order_35971";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "liberty_order_35971",
+  url: "https://liberty_order_35971.botics.co",
+  api: "https://liberty_order_35971.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
